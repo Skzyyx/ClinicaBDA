909823296d3e561b86b5c3f37c9128a6eaf3d672
Skzyy_ <jose.islas252574@potros.itson.edu.mx>
2025-02-19 21:19:06 -0600

    Update PruebasPersistencia.java
    Corrección de un error que hubo al hacer el commit anterior.

--- Codigo java/capaPersistencia/src/main/java/com/mycompany/capapersistencia/PruebasPersistencia.java
+++ Codigo java/capaPersistencia/src/main/java/com/mycompany/capapersistencia/PruebasPersistencia.java
@@ -1,48 +1,93 @@
-package com.mycompany.capanegocio;
 
-import BO.PacienteBO;
-import DTO.PacienteNuevoDTO;
-import Exception.NegocioException;
-import at.favre.lib.crypto.bcrypt.BCrypt;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capapersistencia;
+
+import DAO.CitaDAO;
+import DAO.PacienteDAO;
 import conexion.ConexionDB;
 import conexion.IConexion;
+import entidades.Cita;
 import entidades.Direccion;
+import entidades.Medico;
+import entidades.Paciente;
 import entidades.Usuario;
 import excepciones.PersistenciaException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author 00000207653 Jesus Octavio Amarillas Amaya 
  * @author 00000252574 Jose Luis Islas Molina 
  * @author 00000253301 Isabel Valenzuela Rocha 
  */
-public class PruebasNegocio {
-    private static final IConexion conexion = new ConexionDB();
-    private static final PacienteBO pacienteBO = new PacienteBO(conexion);
+public class PruebasPersistencia {
+    private static final IConexion conexionDB = new ConexionDB();
+    private static final PacienteDAO pacienteDAO = new PacienteDAO(conexionDB);
+    private static final CitaDAO citaDAO = new CitaDAO(conexionDB);
     
-    public static void main(String[] args) throws PersistenciaException, NegocioException {
-        pruebaRegistrarPaciente();
-        pruebaEncriptarContrasenia();
+    public static void main(String[] args) throws SQLException {
+        //pruebaConexion();
+        //pruebaInsertarPaciente();
+        //pruebaConsultarPacientePorId();
+        //pruebaEditarDatosPaciente();
+        //pruebaVerPerfilPaciente();
+        pruebaConsultarPacientePorEmail();
     }
     
     /**
-     * Prueba registrar paciente
+     * Prueba de conexión
      */
-    private static void pruebaRegistrarPaciente() throws NegocioException{
+    private static void pruebaConexion() throws SQLException {
+        try (Connection con = conexionDB.crearConexion()) {
+            if (con != null && !con.isClosed()) {
+                System.out.println("Conexión exitosa a la base de datos.");
+            }
+            else {
+                System.out.println("No se pudo establecer la conexión.");
+            }
+        } catch (PersistenciaException e) {
+            System.out.println("Error en la conexión: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Prueba insertar paciente
+     */
+    private static void pruebaInsertarPaciente() {
         try {
             // Crear instancia de Usuario
-            Usuario usuario = new Usuario("anaPerez123@gmail.com", "seguraClave2024", "PACIENTE");
+            Usuario usuario = new Usuario("maria.gomez@example.com", "claveSegura456", "PACIENTE");
 
             // Crear instancia de Dirección
-            Direccion direccion = new Direccion("Calle Reforma", "102", "Centro", "85000");
+            Direccion direccion = new Direccion("Reforma", "789", "Centro", "06010");
 
-            PacienteNuevoDTO pacienteAGuardar = new PacienteNuevoDTO("Ana", "Pérez", "López", LocalDate.of(1992, 8, 25), "anaPerez123@gmail.com", "6621457890", usuario, direccion);
-
-            boolean pacienteRegistrado = pacienteBO.registrarPaciente(pacienteAGuardar);
+            // Crear instancia de Paciente
+            Paciente paciente = new Paciente(
+                    "María",
+                    "Gómez",
+                    "Hernández",
+                    LocalDate.of(1995, 5, 10),
+                    "maria.gomez@example.com",
+                    "5556781234",
+                    usuario,
+                    direccion
+            );
             
-            if (pacienteRegistrado) {
+            boolean pacienteGuardado = pacienteDAO.registrarPaciente(paciente);
+            
+            if (pacienteGuardado) {
                 System.out.println("Paciente registrado con éxito.");
-            } else {
+            }
+            else {
                 System.out.println("No se registró el paciente.");
             }
         } catch (PersistenciaException e) {
@@ -51,21 +96,157 @@ public class PruebasNegocio {
     }
     
     /**
-     * Prueba de encriptar contraseña
+     * Prueba consultar paciente por id
      */
-    private static void pruebaEncriptarContrasenia() {
-        String password = "1234";
-        String bcryptHashString = BCrypt.withDefaults().hashToString(12, password.toCharArray());
-        System.out.println(bcryptHashString);
-        
-        // Compara la contraseña en texto (password.toCharArray()) con el hash (bcryptHashString)
-        BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), bcryptHashString);
-        
-        // Comprobar si la contraseña es correcta
-        if (result.verified) {
-            System.out.println("Contraseña correcta.");
-        } else {
-            System.out.println("Contraseña incorrecta.");
+    private static void pruebaConsultarPacientePorId() {
+        int id = 30;
+        try {
+            Paciente paciente = pacienteDAO.consultarPacientePorId(id);
+
+            if (paciente == null) {
+                System.out.println("No existe el paciente");
+            }
+        } catch (PersistenciaException e) {
+            System.out.println(e.getMessage());
         }
+    }
+    
+    /**
+     * Prueba consultar paciente por email
+     */
+    private static void pruebaConsultarPacientePorEmail() {
+        String email = "maria.gomez@example.com";
+        
+        try {
+            Paciente paciente = pacienteDAO.consultarPacientePorEmail(email);
+
+            if (paciente == null) {
+                System.out.println("No existe el paciente");
+            }
+        } catch (PersistenciaException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    /**
+     * Prueba editar datos paciente
+     */
+    private static void pruebaEditarDatosPaciente() {
+        try {
+            // Crear instancia de Usuario
+            Usuario usuario = new Usuario("maria.gomez@example.com", "claveSegura456", "PACIENTE");
+
+            // Crear instancia de Dirección
+            Direccion direccion = new Direccion("Veracuz", "789", "Centro", "06010");
+
+            // Crear instancia de Paciente
+            Paciente paciente = new Paciente(
+                    10,
+                    "María",
+                    "Gómez",
+                    "Perez",
+                    LocalDate.of(1995, 5, 10),
+                    "maria.gomez@example.com",
+                    "6442635874",
+                    usuario,
+                    direccion
+            );
+
+            boolean pacienteEditado = pacienteDAO.editarDatosPaciente(paciente);
+            
+            if (pacienteEditado) {
+                System.out.println("Datos editados con éxito");
+            } else {
+                System.out.println("No se editaron los datos");
+            }
+        } catch (PersistenciaException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    /**
+     * Prueba ver perfil de paciente
+     */
+    private static void pruebaVerPerfilPaciente() {
+        try {
+            int id = 1;
+            Paciente pacientePerfil = pacienteDAO.verPerfilPaciente(id);
+            
+            if (pacientePerfil != null) {
+                System.out.println("Perfil encontrado");
+            }
+            else {
+                System.out.println("No se obtuvo perfil");
+            }
+        } catch (PersistenciaException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    private static void pruebaObtenerCitasActivasPorId(int id) throws SQLException {
+        System.out.println("--------------------");
+        String sql = "CALL obtenerCitasActivasPorId(?)";
+        try (Connection con = conexionDB.crearConexion();
+                CallableStatement cs = con.prepareCall(sql)) {
+            
+            cs.setInt(1, id);
+           
+            ResultSet rs = cs.executeQuery();
+            
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt(1) + ", " +
+                        rs.getString(2) + ", " +
+                        rs.getString(3) + ", " + 
+                        rs.getString(4) + ", " + 
+                        rs.getString(5) + ", " + 
+                        rs.getInt(6) + ", " + 
+                        rs.getInt(7)
+                );
+            }
+        } catch (PersistenciaException ex) {
+            Logger.getLogger(PruebasPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static void pruebaRegistrarCitaProgramada() throws SQLException {
+        
+        try {
+            // Crear instancia de Usuario
+            Usuario usuario = new Usuario(1, "juan.perez@example.com", "claveSegura456", "PACIENTE");
+
+            // Crear instancia de Dirección
+            Direccion direccion = new Direccion("Reforma", "789", "Centro", "06010");
+
+            // Crear instancia de Paciente
+            Paciente paciente = new Paciente(
+                    1,
+                    "Juan",
+                    "Perez",
+                    "Gomez",
+                    LocalDate.of(1990, 05, 14),
+                    "juan.perez@example.com",
+                    "5551234567",
+                    usuario,
+                    direccion
+            );
+            
+            // Crear instancia de Usuario
+            Usuario usuarioMedico = new Usuario(11, "77777777", "claveSegura456", "MEDICO");
+            
+            Medico medico = new Medico(1, "Ricardo", "Castillo", "Reyes", "Cardiología", "11111111", "ACTIVO", usuarioMedico);
+            
+            Cita cita = new Cita(LocalDateTime.of(2025, 02, 19, 18, 24, 54), null, "12345678", "PROGRAMADA", paciente, medico);
+            
+            boolean resultado = citaDAO.registrarCitaProgramada(cita);
+            
+            if (resultado) {
+                System.out.println("Se pudo agregar la cita.");
+            } else {
+                System.out.println("Se pudo agregar la cita: ");
+            }
+        } catch (PersistenciaException ex) {
+            Logger.getLogger(PruebasPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        } 
     }
 }
