82bfe272cf8f4b31b24c4063b3784548c617219d
Isabel Valenzuela Rocha <isabel.valenzuela253301@potros.itson.edu.mx>
2025-02-22 16:59:29 -0700

    Update medicoScript.sql
    + Operaciones necesarias para proceso de baja y alta de médico

--- Diseño y modelos/MySQL/Scripts/medicoScript.sql
+++ Diseño y modelos/MySQL/Scripts/medicoScript.sql
@@ -1,4 +1,5 @@
 USE clinicabda;
+
 -- Procedimiento almacenado obtenerMedico
 -- Obtiene la informacion de un medico (y usuario)
 DELIMITER $$
@@ -25,7 +26,8 @@ BEGIN
 END $$
 DELIMITER ;
 
--- Procedimiento para obtener Medico por ID
+-- Procedimiento consultarHorariosMedicoPorID
+-- Consultar los horarios de un médico con un id específico
 DELIMITER &&
 CREATE PROCEDURE consultarHorariosMedicoPorID(
 IN id_Medico int)
@@ -36,4 +38,54 @@ FROM horarios as h
 WHERE h.idMedico=id_Medico;
 
 END &&
+DELIMITER ;
+
+-- Función contarMedicosActivos
+-- Cuenta la cantidad de médicos activos
+DELIMITER $$
+CREATE FUNCTION contarMedicosActivos()
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE cantidadMedicosActivos INT;
+    
+    SELECT COUNT(*)
+    INTO cantidadMedicosActivos
+    FROM medicos
+    WHERE estado = "ACTIVO";
+    
+    RETURN cantidadMedicosActivos;
+END $$
+DELIMITER ;
+
+-- Vista vistaMedicosCitasActivas
+-- Muestra los médicos que tienen citas activas y cuántas citas son
+CREATE OR REPLACE VIEW vistaMedicosCitasActivas AS
+SELECT 
+	m.idMedico, 
+    m.cedula,
+    m.estado,
+    COUNT(c.idCita) AS citasActivas
+FROM medicos AS m
+LEFT JOIN citas AS c
+	ON m.idMedico = c.idMedico AND c.estado = "ACTIVA"
+GROUP BY m.idMedico, m.cedula, m.estado;
+
+-- Función citasActivasMedico
+-- Obtiene la cantidad de citas activas de un médico específico
+DELIMITER $$
+CREATE FUNCTION citasActivasMedico(cedulaMedico VARCHAR(8))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE cantidadCitasActivas INT;
+    
+    SELECT 
+		citasActivas
+	INTO cantidadCitasActivas
+	FROM vistaMedicosCitasActivas
+    WHERE cedula = cedulaMedico;
+    
+    RETURN cantidadCitasActivas;
+END $$
 DELIMITER ;
